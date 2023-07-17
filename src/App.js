7409781a83246424083119edf3ec30cae99ebc78
郵差 <twHenryYang@gmail.com>
2023-07-17 21:14:53 +0800

    doc: Advance practice note

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import React, { useState, useEffect } from "react";
 
+// TODO: 接下來跟著文章改為進階版(拆分各個Components)
+// https://molly1024.medium.com/react-一個-crud-增改刪查專案-9874b452ae36#5e9b
+
 function App() {
   const usersData = [
     { id: 1, name: "Tania", username: "floppydiskette" },
