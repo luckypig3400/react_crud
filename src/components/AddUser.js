d8ea918c16612790e02ad5fd099676f6206be776
郵差 <twHenryYang@gmail.com>
2023-07-18 21:44:02 +0800

    feat: 完成新增功能(讀取name與產生uuid)

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,25 +1,35 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { Form, FormGroup, Label, Input, Button } from "reactstrap";
 import { Link, useNavigate } from "react-router-dom";
 import { GlobalContext } from '../context/GlobalState';
+import { v4 as uuid } from "uuid";
 
 function AddUser() {
   const { addUser } = useContext(GlobalContext);
   const navigate = useNavigate();
+  const [name, setName] = useState("");
   const onSubmitFunc = () => {
     const newUser = {
-      id: 4,
-      name: "User Four",
+      id: uuid(),
+      name: name,
     };
     addUser(newUser);
     navigate("/");
   };
+  const onChangeFunc = (e) => {
+    setName(e.target.value);
+  }
 
   return (
     <Form onSubmit={onSubmitFunc}>
       <FormGroup>
         <Label>Name</Label>
-        <Input type="text" placeholder="Enter name" />
+        <Input
+          type="text"
+          value={name}
+          onChange={onChangeFunc}
+          placeholder="Enter name"
+        />
       </FormGroup>
       <Button type='submit'>Submit</Button>
       <Link to="/" className='btn btn-danger ml-2'>
