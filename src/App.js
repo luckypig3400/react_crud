29f2b5645f4b2a3a9d9464ceac38f2d27ce4945a
郵差 <twHenryYang@gmail.com>
2023-07-18 18:39:01 +0800

    feat: 套用Bootstrap CSS與新增AddUser表單

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home';
 import AddUser from './components/AddUser';
 import EditUser from './components/EditUser';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // TODO: 接下來跟著文章改為進階版(拆分各個Components)
 // https://molly1024.medium.com/react-一個-crud-增改刪查專案-9874b452ae36#5e9b
--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Form, FormGroup, Label, Input, Button } from "reactstrap";
+import { Link } from "react-router-dom";
 
 function AddUser() {
   return (
-    <div>
-      <h2>Add User</h2>
-    </div>
+    <Form>
+      <FormGroup>
+        <Label>Name</Label>
+        <Input type="text" placeholder="Enter name" />
+      </FormGroup>
+      <Button type='submit'>Submit</Button>
+      <Link to="/" className='btn btn-danger ml-2'>
+        Cancel
+      </Link>
+    </Form >
   )
 }
 
