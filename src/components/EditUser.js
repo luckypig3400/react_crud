f09e84f4393978b55f906e35a5917ba6131bbcca
郵差 <twHenryYang@gmail.com>
2023-07-18 19:29:43 +0800

    feat: 新增EditUser表單

--- src/components/EditUser.js
+++ src/components/EditUser.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Form, FormGroup, Label, Input, Button } from "reactstrap";
+import { Link } from "react-router-dom";
 
 function EditUser() {
   return (
-    <div>
-      <h2>Edit User</h2>
-    </div>
+    <Form>
+      <FormGroup>
+        <Label>Name</Label>
+        <Input type='text' placeholder='Enter Name' />
+      </FormGroup>
+      <Button type='submit'>Submit</Button>
+      <Link to="/" className='btn btn-danger ml-2'>
+        Cancel
+      </Link>
+    </Form>
   )
 }
 
