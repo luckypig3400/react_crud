8450f1a41411cbd5452c2be30a7f7d3bbbd8ae97
郵差 <twHenryYang@gmail.com>
2023-07-18 18:11:08 +0800

    feat: React Router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 import Home from './components/Home';
 import AddUser from './components/AddUser';
 import EditUser from './components/EditUser';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // TODO: 接下來跟著文章改為進階版(拆分各個Components)
 // https://molly1024.medium.com/react-一個-crud-增改刪查專案-9874b452ae36#5e9b
@@ -10,9 +11,14 @@ import EditUser from './components/EditUser';
 function App() {
   return (
     <div className="App">
-      <Home />
-      <AddUser />
-      <EditUser />
+      <Router>
+        <h1>Nav</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/add" element={<AddUser />} />
+          <Route path="/edit" element={<EditUser />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
